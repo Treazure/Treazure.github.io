a7e2a4baac3af69caef3f21b2238c7867216be55
Treazure <44760644+Treazure@users.noreply.github.com>
2018-11-04 21:17:03 -0600

    Initial Launch of 360 App

---
+++ data.js
@@ -0,0 +1,534 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-nevar-dr",
+      "name": "Nevar Dr",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1944,
+      "initialViewParameters": {
+        "yaw": 2.5488608705212474,
+        "pitch": -0.02163411317356001,
+        "fov": 1.4261791587875234
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.5708895737625426,
+          "pitch": 0.33535645315943974,
+          "rotation": 0,
+          "target": "1-02"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-02",
+      "name": "02",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1944,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.34306057649350485,
+          "pitch": 0.5832083771502212,
+          "rotation": 0,
+          "target": "2-03"
+        },
+        {
+          "yaw": 2.642596556019156,
+          "pitch": 0.4218471571011193,
+          "rotation": 0,
+          "target": "0-nevar-dr"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-03",
+      "name": "03",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1944,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.8736974379647497,
+          "pitch": 0.4288451173376444,
+          "rotation": 0,
+          "target": "3-04"
+        },
+        {
+          "yaw": -0.07463081140700822,
+          "pitch": 0.5025055995471988,
+          "rotation": 0,
+          "target": "1-02"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-04",
+      "name": "04",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1944,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.5080306467981117,
+          "pitch": 0.3187624676326557,
+          "rotation": 0,
+          "target": "4-05"
+        },
+        {
+          "yaw": -0.20771598866758723,
+          "pitch": 0.45803674482920975,
+          "rotation": 0,
+          "target": "2-03"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-05",
+      "name": "05",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1944,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.8561486659697977,
+          "pitch": 0.6681877745725195,
+          "rotation": 0,
+          "target": "3-04"
+        },
+        {
+          "yaw": 0.8244507252762432,
+          "pitch": 0.6758806252558731,
+          "rotation": 0,
+          "target": "5-stake-01"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-stake-01",
+      "name": "Stake 01",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1944,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.858304300273776,
+          "pitch": 0.4663516853451739,
+          "rotation": 0,
+          "target": "6-stake-02"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.904496007237503,
+          "pitch": 0.9769122642835306,
+          "title": "Lot Stake",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "6-stake-02",
+      "name": "Stake 02",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1944,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.7983609113106107,
+          "pitch": 0.026314731787779877,
+          "rotation": 0,
+          "target": "5-stake-01"
+        },
+        {
+          "yaw": 0.4520244947081302,
+          "pitch": 0.30429101664678626,
+          "rotation": 0,
+          "target": "7-08"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.7086368444539737,
+          "pitch": 0.9792849531246084,
+          "title": "Lot Stake",
+          "text": "https://www.google.com/maps/embed?pb=!1m17!1m11!1m3!1d321.5067773540504!2d-89.71685686405749!3d43.5856333696182!2m2!1f243.6656049412212!2f0!3m2!1i1024!2i768!4f35!3m3!1m2!1s0x8807476c5e1c81a3%3A0xbdc5a31b0f9d8559!2zNDPCsDM1JzA4LjEiTiA4OcKwNDInNTkuOCJX!5e1!3m2!1sen!2sus!4v1541363428904"
+        },
+        {
+          "yaw": -2.4791316204438907,
+          "pitch": -0.15873073943721394,
+          "title": "House",
+          "text": "N8465 Nevar Dr"
+        },
+        {
+          "yaw": 2.224328987578792,
+          "pitch": 0.42265623277733866,
+          "title": "Lot 693",
+          "text": "Text"
+        },
+        {
+          "yaw": 1.3280168071642855,
+          "pitch": 0.32441859506905146,
+          "title": "Lot 682",
+          "text": "Text"
+        },
+        {
+          "yaw": 2.248604652945259,
+          "pitch": -0.013711926441274613,
+          "title": "Lot 692",
+          "text": "Text"
+        },
+        {
+          "yaw": -0.7257281887207938,
+          "pitch": 0.26925807337880414,
+          "title": "Lot 694",
+          "text": "Text"
+        },
+        {
+          "yaw": -0.20599775368739515,
+          "pitch": 0.2853952424309014,
+          "title": "Lot 682",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "7-08",
+      "name": "08",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1944,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.825997914371671,
+          "pitch": 0.40940392257796177,
+          "rotation": 0,
+          "target": "6-stake-02"
+        },
+        {
+          "yaw": 1.2123104553594004,
+          "pitch": 0.37651897481215,
+          "rotation": 0,
+          "target": "5-stake-01"
+        },
+        {
+          "yaw": -1.47294100289783,
+          "pitch": 0.5314121366916318,
+          "rotation": 0,
+          "target": "8-09"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-09",
+      "name": "09",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1944,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.9171591058287305,
+          "pitch": 0.3269251873397323,
+          "rotation": 0,
+          "target": "7-08"
+        },
+        {
+          "yaw": -0.5628646278546832,
+          "pitch": 0.3070547319519008,
+          "rotation": 0,
+          "target": "9-10"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-10",
+      "name": "10",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1944,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.734476519598978,
+          "pitch": 0.398073110553959,
+          "rotation": 0,
+          "target": "8-09"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-valley-view-ct",
+      "name": "Valley View Ct",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1944,
+      "initialViewParameters": {
+        "yaw": -0.44033616463475767,
+        "pitch": 0.1336121215291648,
+        "fov": 1.4261791587875234
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.7513553542185463,
+          "pitch": 0.479959485786285,
+          "rotation": 0,
+          "target": "9-10"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.23238460644570402,
+          "pitch": -0.11429872087058257,
+          "title": "Road",
+          "text": "Valley View Court is just up here"
+        }
+      ]
+    }
+  ],
+  "name": "Lots 682 & 692",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
